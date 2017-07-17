8ee34e9fd5aaecdafd053a469d2bdf3fba3c627b
Jeremy Taylor <tay16036@byui.edu>
2017-07-17 03:45:51 -0600

    7/17/2017 Created and modified files from tutorial for GeekQuiz

--- GeekQuiz/Global.asax.cs
+++ GeekQuiz/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using GeekQuiz.Models;
 
 namespace GeekQuiz
 {
@@ -13,7 +14,9 @@ namespace GeekQuiz
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+			System.Data.Entity.Database.SetInitializer(new TriviaDatabaseInitializer());
+
+			AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
